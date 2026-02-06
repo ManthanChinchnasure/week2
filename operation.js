40ab0f14bf45f93f02deccd4413ac1a2e0955974
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-06 22:27:36 +0530

    Js operation

---
+++ operation.js
@@ -0,0 +1,26 @@
+console.log("Operation in js"); 
+
+let a = 4;
+let b = 4;
+
+// console.log("a + b  = ", a+b )
+// console. log("a - b = ", a-b)
+// console.log("a * b = ",a * b)                  it is a basic operation to perform the task.
+// console .log("a / b = ", a/b)
+// console.log("a % b = ", a % b)    module is know as the reminder
+// console.log("a**b= ", a**b)
+
+
+
+
+console.log("++a = ", ++a)    // in it pre  the value of "a" is already define than display..  // 5
+
+console.log("a++ = ", a++)   // increment    post incrment  "a" is value define {after} new value is add leter than the original value display.//5
+
+console.log(a) //6
+
+
+console.log("--b=", --b)  // pre drecement frist the value is decreases than disply //3
+console.log("b-- = ", b--)    //  post decrement the value decreases  {after} it disply the dereases value. //3 which define the value leter on it.
+console.log(b)    // decrement  //2
+
