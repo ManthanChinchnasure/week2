3a7ff668f0c1198759ceff9503ec8d779f51ec4e
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-14 13:13:14 +0530

    loop

---
+++ loop.js
@@ -0,0 +1,47 @@
+// for loop                      for(stat1,stat2,stat3){
+                        //        console.log();
+                         //      }
+  // in the condition the value of stat1 is n and the the value of statment 2 is less by n-1.....
+
+
+
+   /*
+   for (i = 0; i< 50; i++ ) {
+    console.log(i)                           // if can count form i+1 or i+2 whatever in it...c
+
+  }
+
+  */
+
+  /*
+  let sum = 0;
+  let n = prompt("what is the value of the sum");
+  n = Number.parseInt(n);
+  for(i=0; i<5; i++){
+    sum += i
+    console.log(i, "+")
+  }
+
+  console.log("sum of frist"+ n + "sum of toatal " + sum );
+
+*/
+
+
+// for in loop
+
+let obj = {
+    shivi : 34,                      // only use of it when we have get a keys....
+    bb : 34 ,
+    yt : 45,
+}
+
+for (let a in obj){
+    console.log("which is obj"+ a + "mark" + obj[a])
+
+}
+
+
+
+// for of loop
+
+// expolr the it..
