4dcf6b13d15a7d1bf70b896b4ccd5bf010de5445
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-14 14:37:52 +0530

    improve

---
+++ function.js
@@ -0,0 +1,11 @@
+function sum(a,b){
+    sum = a+b;
+    return sum 
+}                                         // it written without help of video watching only one time see the video than i write the code 
+                                          // little bit improve ....
+
+let a =3;
+let b = 4;
+let c = 5;
+
+console.log(  " the sum ", sum(b,c));
