cd042af2b745bca7f8aa002af1bb32f19e8b5fe6
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-20 14:47:39 +0530

    map filter,arrow funcation

--- week2.1/arrays.js
+++ week2.1/arrays.js
@@ -35,3 +35,62 @@ const ans = sum(1,3)
 console.log(ans);
 
 */
+
+/*
+// map
+
+// given an array which multiplay it by 2 give back a newarray..
+ 
+ let input = [1,2,3,4,5];
+
+//soluation
+const newarray =[]
+
+for( let i=0; i<input.length; i++){                      it a basic soluation of it now below we used the map of array..
+    newarray .push(input[i]*2)
+
+}
+
+console.log(newarray);
+*/
+
+// other soluation
+
+// let input1 =[1,2,3,4,5];
+ 
+// const ans = input1.map(function(i){
+//     return i*3
+// })
+
+// console.log(ans);
+
+
+
+// filtering
+//  in given input the values give back a even values..
+/*
+const input3 =[1,2,3,4,5]
+
+const newarray=[]
+
+for (let i = 0; i<input3.length; i++){
+    if(input3[i]%2 ==0){
+        newarray.push(input3[i]);
+
+    }
+}
+console.log(newarray);
+
+*/
+
+const are = [1,2,3,4,5]
+
+const ans = are.filter(function(n) {
+    if(n%2 == 0){
+        return true;
+     } else {
+            return false;
+        }
+ });
+
+ console.log(ans);
