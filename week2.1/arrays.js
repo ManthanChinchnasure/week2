1886202d9362c12d246a6852f5e5d554cf820bdf
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-20 11:31:10 +0530

    array

---
+++ week2.1/arrays.js
@@ -0,0 +1,37 @@
+
+/*
+
+let sk = [30,4,5,5,4,2,]
+console.log(sk);
+console.log(sk[0]);
+console.log(sk.length)
+sk[0]=91
+console.log(sk);
+
+
+*/
+
+// arrow function
+
+/*
+
+function sum(a,b){          on the funcation we are discribe it as it
+    return a + b                       |
+}                                        |  the below statment is the arrow function which some of advance topic of arrow funcation....
+
+
+
+
+
+const sum = (a,b) =>{
+    return a + b
+}
+ app.get("/" ,( req , res ) => {
+
+ })
+
+
+const ans = sum(1,3)
+console.log(ans);
+
+*/
---
+++ week2.1/string.js
@@ -0,0 +1,23 @@
+let name = "manthan"
+//console.log(name.length);
+console.log(name);
+
+//console.log(name.slice (2));
+
+let b = "shuhant 10"
+
+//console.log(name.concat('he is doing not expected  ', b  ,"  let understand the thing"))
+
+console.log(b[0]);
+console.log(b[3]);
+
+
+for (i=7; i>6; i--){
+   // console.log(b.concat("the name is", name,"let ti is"));
+   console.log(b[4]);
+}
+
+
+// let amount= Number.parseInt(b)
+// console.log(amount);
+// console.log(typeof amount)
